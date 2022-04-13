d848097023251670b144cdc8ac5aeb302cae482d
Darren Grafton <54216318+DarrenGrafton@users.noreply.github.com>
2022-04-13 15:16:19 -0500

    Update rollup.config.js

--- SvelteRazorViewsUMD/rollup.config.js
+++ SvelteRazorViewsUMD/rollup.config.js
@@ -1,7 +1,5 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
-import json from '@rollup/plugin-json';
-import commonjs from '@rollup/plugin-commonjs';
 import css from 'rollup-plugin-css-only';
 
 export default {
@@ -23,7 +21,5 @@ export default {
             browser: true,
             dedupe: ["svelte"]
         }),
-        json(),
-        commonjs(),
     ]
 };
